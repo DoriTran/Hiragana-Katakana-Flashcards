2caafcd58685d7ea13359e4a5e070a3ed684cf5b
DoriTran <tranquocdong308@gmail.com>
2024-11-26 11:47:33 +0700

    add init learning page

---
+++ src/features/learning/FlashCard/FlashCard.js
@@ -0,0 +1,7 @@
+import { Paper } from "@mui/material";
+
+const FlashCard = ({ front, back, flipped = false }) => {
+  return <Paper elevation={4}>{children}</Paper>;
+};
+
+export default FlashCard;
